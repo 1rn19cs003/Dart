71a77473972a5a81c4b547d7bae73c8aff2b53f7
abhishek <abhicse003@gmail.com>
2021-09-18 01:03:02 +0530

    update dart

--- first.dart
+++ first.dart
@@ -6,12 +6,30 @@
 //   }
 // }
 // ----------------------Second-------------------
-void main() {
-  String name = 'Abhishek jaiswal';
-  print("My name is $name");
-  print("My name is ${name.length}");
-}
+// void main() {
+// String name = 'Abhishek jaiswal';
+// print("My name is $name");
+// print("My name is ${name.length}");
 // ----------------------Third-------------------
+// }
+// variales ---:
+//  ---> final,var,const,normal
+// ------>optional parameter
+// void main() {
+  // var name = 'abhishek jaiswal';
+  // var age = 21;
+  // var height = 5.10;
+  // final persion1 = describe(name, age, height);
+  // final persion2 = describe("Keerthi", 20);
+  // print(persion1);
+  // print(persion2);
+// }
+// 
+// String describe(String name, int age, [double height = 6.10]) {
+  // return "my name is ${name} age is ${age}, and height is ${height}";
+// }
+// ------------>Arrow Operator
+// ------------>OOPS
 // ----------------------Fourth-------------------
 // ----------------------Five-------------------
 // ----------------------Six-------------------
